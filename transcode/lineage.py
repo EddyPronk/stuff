a653bd0edc27a0b480939f952309209ae92ffcdd
Eddy Pronk <epronk@godiva.(none)>
2007-11-13 07:59:00 +1100

    moved code from test to module

--- transcode/lineage.py
+++ transcode/lineage.py
@@ -1,84 +1,85 @@
 import re
 import string
 import os
+import album
 
-class Track:
-    def __init__(self, number):
-        self.number = number
-        self.checksum = ''
-        pass
-
-class Album:
+class Lineage:
     def __init__(self):
-        self.tracks = {}
+        self.r = []
+        self.album = album.Album()
+        self.disk = self.album.disk(1)
 
-    def track(self, track_number):
-        if self.tracks.has_key(track_number):
-            return self.tracks[track_number]
-        else:
-            self.tracks[track_number] = Track(track_number)
-            return self.tracks[track_number]
+        self.checksums = []
+        self.add(('^\s*([0-9]+).\s+(.*)',   lambda r : self.insertTitle(r.group(1), r.group(2))))
+        self.add(('([^:]+):([0-9a-f]+)', lambda r : self.insertChecksum(r.group(1), r.group(2))))
+        self.add(('([0-9a-f]+) \*(.*)',  lambda r : self.insertChecksum(r.group(2), r.group(1))))
+        self.add(('^DISC ([0-9])',       lambda r : self.insertAlbum(r.group(1))))
 
-    def do_print(self):
-        file = open('album.cue', 'w')
+    def extract(self):
+        album = Album()
+        file = open("montreux.txt")
+        for line in file.readlines():
+            parse(line)
+        album.do_print()
+
+    def readlines(self):
+        for line in self.input_file.readlines():
+            print line
+            self.try_match(line)
+
+    def insertAlbum(self, disk_number):
+        self.disk = self.album.disk(string.atoi(disk_number))
+
+    def insertTitle(self,number, title):
+        track_number = string.atoi(number)
+        track = self.disk.track(track_number)
+        track.title = " ".join([ word.capitalize() for word in title.split() ])
+        track.title = track.title.replace(' / ', ', ')
+        print '%s-%s' % (track_number, track.title)
+
+    def insertChecksum(self,filename, checksum):
+        self.checksums.append((filename, checksum))
+
+    def write_cue_sheet(self):
+        file = self.file
         file.write('PERFORMER ""\n')
         file.write('TITLE ""\n')
         file.write('YEAR ""\n')
         file.write('COMMENT "LAME 3.97 (--preset standard)"\n')
         file.write('\n')
-        for track in self.tracks.values():
+        for track in self.album.disk(1).tracks.values():
             #file.write('TRACK %s AUDIO\n' % track.number)
             file.write('TRACK %s\n' % track.number)
             file.write('  TITLE "%s"\n' % track.title)
             #file.write('  FILE "%s" FLAC\n' % track.filename)
-            file.write('  FILE "%s"\n' % track.filename)
-            file.write('  CHECKSUM "%s"\n' % track.checksum)
+            #file.write('  FILE "%s"\n' % track.filename)
+            #file.write('  CHECKSUM "%s"\n' % track.checksum)
 
-def extract():
-    album = Album()
-    file = open("montreux.txt")
-    for line in file.readlines():
-        line = line.replace('\r', '')
-        line = line.replace('\n', '')
-        expr = r'^([0-9]+).\s+(.*)'
-        res = re.search(expr, line)
-        if res is not None:
-            track_number = string.atoi(res.group(1))
-            track = album.track(track_number)
-            track.title = " ".join([ word.capitalize() for word in res.group(2).split() ])
-            track.title = track.title.replace(' / ', ', ')
-        expr = r'([^:]+):([0-9a-f]+)'
-        res = re.search(expr, line)
-        if res is not None:
-            filename = res.group(1)
-            checksum = res.group(2)
-            res = re.search(r'([0-9]+)[-](.*).flac', filename)
-            if res is not None:
-                track_number = string.atoi(res.group(1))
-                track = album.track(track_number)
-                track.filename = filename
-                track.checksum = checksum
-            res = re.search(r'Track([0-9]+).flac', filename)
-            if res is not None:
-                track_number = string.atoi(res.group(1))
-                track = album.track(track_number)
-                track.filename = filename
-                track.checksum = checksum
-            # 0ed7b426c32f30ffc6235ec09783448e *Prince2007-07-16_t15.flac
+    def find_track_number(self):
+        i = 0
+        res = 0
+        while res == 0:
+            c = self.checksums[0][0][i]
+            for s in self.checksums:
+                if c != s[0][i]:
+                    res = i
+                    break
+            i += 1
+        for s in self.checksums:
+            track_number = string.atoi(re.search('([0-9]+)', s[0][res:]).group(1))
+            track = self.album.disk(1).track(track_number)
+            track.filename = s[0]
+            track.checksum = s[1]
+            track = self.album.disk(1).track(track_number)
 
-        expr = r'([0-9a-f]+) \*(.*)'
-        res = re.search(expr, line)
-        if res is not None:
-            filename = res.group(2)
-            checksum = res.group(1)
-            print res.group(2)
-            res = re.search(r'.*_t([0-9]+).flac', filename)
-            if res is not None:
-                track_number = string.atoi(res.group(1))
-                print track_number
-                track = album.track(track_number)
-                track.filename = filename
-                track.checksum = checksum
-    album.do_print()
+    def add(self, entry):
+        self.r.append(entry)
 
-extract()
+    def try_match(self, line):
+        for t in self.r:
+            expr,b = t
+            res = re.search(expr, line)
+            if res is not None:
+                b(res)
+                break
+
--- transcode/test_lineage.py
+++ transcode/test_lineage.py
@@ -2,91 +2,12 @@ import unittest
 import re
 import string
 import album
-
-class Lineage:
-    def __init__(self):
-        self.r = []
-        self.album = album.Album()
-        self.disk = self.album.disk(1)
-
-        self.checksums = []
-        self.add(('^\s*([0-9]+).\s+(.*)',   lambda r : self.insertTitle(r.group(1), r.group(2))))
-        self.add(('([^:]+):([0-9a-f]+)', lambda r : self.insertChecksum(r.group(1), r.group(2))))
-        self.add(('([0-9a-f]+) \*(.*)',  lambda r : self.insertChecksum(r.group(2), r.group(1))))
-        self.add(('^DISC ([0-9])',       lambda r : self.insertAlbum(r.group(1))))
-
-    def extract(self):
-        album = Album()
-        file = open("montreux.txt")
-        for line in file.readlines():
-            parse(line)
-        album.do_print()
-
-    def readlines(self):
-        for line in self.input_file.readlines():
-            print line
-            self.try_match(line)
-
-    def insertAlbum(self, disk_number):
-        self.disk = self.album.disk(string.atoi(disk_number))
-
-    def insertTitle(self,number, title):
-        track_number = string.atoi(number)
-        track = self.disk.track(track_number)
-        track.title = " ".join([ word.capitalize() for word in title.split() ])
-        track.title = track.title.replace(' / ', ', ')
-        print '%s-%s' % (track_number, track.title)
-
-    def insertChecksum(self,filename, checksum):
-        self.checksums.append((filename, checksum))
-
-    def write_cue_sheet(self):
-        file = self.file
-        file.write('PERFORMER ""\n')
-        file.write('TITLE ""\n')
-        file.write('YEAR ""\n')
-        file.write('COMMENT "LAME 3.97 (--preset standard)"\n')
-        file.write('\n')
-        for track in self.album.disk(1).tracks.values():
-            #file.write('TRACK %s AUDIO\n' % track.number)
-            file.write('TRACK %s\n' % track.number)
-            file.write('  TITLE "%s"\n' % track.title)
-            #file.write('  FILE "%s" FLAC\n' % track.filename)
-            #file.write('  FILE "%s"\n' % track.filename)
-            #file.write('  CHECKSUM "%s"\n' % track.checksum)
-
-    def find_track_number(self):
-        i = 0
-        res = 0
-        while res == 0:
-            c = self.checksums[0][0][i]
-            for s in self.checksums:
-                if c != s[0][i]:
-                    res = i
-                    break
-            i += 1
-        for s in self.checksums:
-            track_number = string.atoi(re.search('([0-9]+)', s[0][res:]).group(1))
-            track = self.album.disk(1).track(track_number)
-            track.filename = s[0]
-            track.checksum = s[1]
-            track = self.album.disk(1).track(track_number)
-
-    def add(self, entry):
-        self.r.append(entry)
-
-    def try_match(self, line):
-        for t in self.r:
-            expr,b = t
-            res = re.search(expr, line)
-            if res is not None:
-                b(res)
-                break
+import lineage
 
 class TestLineage(unittest.TestCase):
 
     def setUp(self):
-        self.lineage = Lineage()
+        self.lineage = lineage.Lineage()
     
     def testDisk(self):
         disk = self.lineage.album.disk(1)
