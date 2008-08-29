443ff72cdadab3ad11857277f4e284613475c2d4
Eddy Pronk <epronk@muftor.com>
2008-08-29 23:24:14 +1000

    refactor tests

--- pyfit/runtests.py
+++ pyfit/runtests.py
@@ -1,6 +1,7 @@
 import unittest
 from test_table import *
 from test_import import *
+from test_fixtures import *
 
 if __name__ == '__main__':
     unittest.main()
--- pyfit/test_table.py
+++ pyfit/test_table.py
@@ -18,24 +18,6 @@ class TradingStart(object):
     def run(self):
         pass
 
-class TestActionFixture(ActionFixture):
-
-    trace = []
-    def amount(self, x):
-        self.trace.append(['amount', 'x', x])
-
-    def user(self, userName):
-        self.trace.append(['user', 'userName', userName])
-
-    def add(self, x, y):
-        self.trace.append(['add', 'x', x, 'y', y])
-
-            
-class TestDoFixture(DoFixture):
-    trace = []
-    def UserCreatesRoom(self, userName, roomName):
-        self.trace.append(['UserCreatesRoom', "userName", userName, "roomName", roomName])
-
 class TestTable(unittest.TestCase):
 
     def testSignature(self):
@@ -187,48 +169,6 @@ class TestTable(unittest.TestCase):
 
         self.assertEqual('CalculateDiscount', str(name))
         #print table.doc.toxml()
-
-    def test_action_fixture2(self):
-        table = '''
-            |TestActionFixture|
-            |enter|user  |anna|
-            |check|amount|24|
-            |check|add   |12|7|
-        '''
-        
-        table = Table(wiki_table_to_html(table))
-        rows = table.rows()
-        row = rows.next()
-        it = RowIter(iter(row))
-        name = it.next() 
-
-        def CreateFixture(name):
-            return globals()[name]()
-
-        fixture = CreateFixture(str(name))
-        fixture.process(table)
-        self.assertEqual(fixture.trace[0], ['user', 'userName', 'anna'])
-        self.assertEqual(fixture.trace[1], ['amount', 'x', '24'])
-        self.assertEqual(fixture.trace[2], ['add', 'x', '12', 'y', '7'])
-
-    def test_do_fixture(self):
-        table = '''
-            |TestDoFixture|
-            |User|anna|Creates|lotr|Room|
-        '''
-        
-        table = Table(wiki_table_to_html(table))
-        rows = table.rows()
-        row = rows.next()
-        it = RowIter(iter(row))
-        name = it.next() 
-
-        def CreateFixture(name):
-            return globals()[name]()
-
-        fixture = CreateFixture(str(name))
-        fixture.process(table)
-        self.assertEqual(fixture.trace[0], ['UserCreatesRoom', 'userName', 'anna', 'roomName', 'lotr'])
 
     def test_td_with_newline(self):
         # I'm using deepest to get the text out.
